3821943ae9f0f4eb90b8f23c19043387f0bd6d00
Temich93s <nil>
2021-10-02 10:51:08 +1000

    Пройдена тема: Ленивые свойства хранения

--- Properties/main.swift
+++ Properties/main.swift
@@ -31,3 +31,29 @@ print("\n//Свойства хранения постоянных экземпл
 let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
 //rangeOfFourItems.firstValue = 6 //ERROR, так как let rangeOfFourItems
 print(rangeOfFourItems.firstValue, rangeOfFourItems.length)
+
+
+//MARK: Ленивые свойства хранения
+print("\n//Ленивые свойства хранения")
+
+class DataImporter {
+    /*
+     DataImporter - класс для импорта данных из внешних источников
+     Считаем, что классу требуется большое количество времени для инициализации
+     */
+    var fileName = "data.txt"
+    // класс DataImporter функционал данных будет описан тут
+}
+ 
+class DataManager {
+    lazy var importer = DataImporter()
+    var data = [String]()
+    // класс DataManager обеспечит необходимую функциональность тут
+}
+ 
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+// экземпляр класса DataImporter для свойства importer еще не создано
+
+print(manager.data)
