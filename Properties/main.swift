ac4ecbbf84f153ff298e5005cc0cdd90b2e78b1f
Temich93s <nil>
2021-10-02 10:32:32 +1000

    Пройдена тема: Свойства хранения

--- Properties/main.swift
+++ Properties/main.swift
@@ -9,3 +9,17 @@ import Foundation
 
 print("Hello, World!")
 
+
+//MARK: Свойства хранения
+print("\n//Свойства хранения")
+
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+print(rangeOfThreeItems.firstValue, rangeOfThreeItems.length)
+
+rangeOfThreeItems.firstValue = 6
+print(rangeOfThreeItems.firstValue, rangeOfThreeItems.length)
