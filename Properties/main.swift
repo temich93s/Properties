8ffebeae193868a4c5bddcea266b2a02b407f5b0
Temich93s <nil>
2021-10-03 08:49:44 +1000

    Пройдена тема: Обертки для свойств

--- Properties/main.swift
+++ Properties/main.swift
@@ -171,3 +171,45 @@ func someFunction(numberA: inout Int) {
 }
 
 someFunction(numberA: &stepCounter.totalStep)
+
+
+//MARK: Обертки для свойств
+print("\n//Обертки для свойств")
+
+@propertyWrapper struct TwelveOrLess {
+    private var number = 0
+    var wrappedValue: Int {
+        get { return number }
+        set { number = min(newValue, 12) }
+    }
+}
+
+struct SmallRectangle {
+    @TwelveOrLess var height: Int
+    @TwelveOrLess var width: Int
+}
+
+//struct SmallRectangle {
+//    private var _height = TwelveOrLess()
+//    private var _width = TwelveOrLess()
+//    var height: Int {
+//        get { return _height.wrappedValue }
+//        set { _height.wrappedValue = newValue }
+//    }
+//    var width: Int {
+//        get { return _width.wrappedValue }
+//        set { _width.wrappedValue = newValue }
+//    }
+//}
+
+var rectangle = SmallRectangle()
+print(rectangle.height)
+// Выведет "0"
+
+rectangle.height = 10
+print(rectangle.height)
+// Выведет "10"
+
+rectangle.height = 24
+print(rectangle.height)
+// Выведет "12"
