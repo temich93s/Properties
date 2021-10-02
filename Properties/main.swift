086296cbe3e7a484c516c6f1300a473c605cf6bb
Temich93s <nil>
2021-10-02 15:11:20 +1000

    Пройдена тема: Вычисляемые свойства

--- Properties/main.swift
+++ Properties/main.swift
@@ -57,3 +57,39 @@ manager.data.append("Some more data")
 // экземпляр класса DataImporter для свойства importer еще не создано
 
 print(manager.data)
+
+
+//MARK: Вычисляемые свойства
+print("\n//Вычисляемые свойства")
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+}
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0), size: Size(width: 10.0, height: 10.0))
+
+let initialSquareCenter = square.center
+print(initialSquareCenter)
+
+square.center = Point(x: 15.0, y: 15.0)
+print(square.origin)
