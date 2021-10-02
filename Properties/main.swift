661e3907558c44b6cb9249bf98e4705182a9cbbe
Temich93s <nil>
2021-10-02 15:23:02 +1000

    Пройдена тема: Сокращенный вариант объявления геттера

--- Properties/main.swift
+++ Properties/main.swift
@@ -113,3 +113,21 @@ struct AlternativeRect {
         }
     }
 }
+
+
+//MARK: Сокращенный вариант объявления геттера
+print("\n//Сокращенный вариант объявления геттера")
+
+struct CompactRect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            Point(x: origin.x + (size.width / 2), y: origin.y + (size.height / 2))
+        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
